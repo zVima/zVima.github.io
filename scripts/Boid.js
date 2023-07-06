0aa01045d3513ca94ccba0bdf3e66dcb67d8d23b
zVima <115565123+zVima@users.noreply.github.com>
2023-07-06 10:28:13 +0200

    Update Boid.js

--- scripts/Boid.js
+++ scripts/Boid.js
@@ -2,38 +2,29 @@
  * A bird-like object, simulating flock behaviour
  */
 class Boid {
-  constructor(position, velocity, size) {
+  constructor(position, velocity, acceleration, color) {
     this.position = position;
     this.velocity = velocity;
-    this.size = size;
+    this.acceleration = acceleration;
+    this.color = color;
   }
 
   // Udpating the boids Position
   update(width, height, turnFactor, offset) {
     if (this.position.x < offset) {
-      this.velocity.x = this.velocity.x + turnFactor;
-    } 
-    if (this.position.x > width - offset) {
-      this.velocity.x = this.velocity.x - turnFactor;
+      this.velocity.x += turnFactor;
+    } else if (this.position.x > width - offset) {
+      this.velocity.x -= turnFactor;
     }
     if (this.position.y > height - offset) {
-      this.velocity.y = this.velocity.y - turnFactor;
-    } 
-    if (this.position.y < offset ) {
-      this.velocity.y = this.velocity.y + turnFactor;
+      this.velocity.y -= turnFactor;
+    } else if (this.position.y < offset) {
+      this.velocity.y += turnFactor;
     }
-    
+
+    this.velocity.add(this.acceleration);
     this.position = this.position.add(this.velocity);
-  }
-
-  // Drawing the Boid onto the display as an Square
-  draw(context) {
-    const x = this.position.x - (this.size / 2);
-    const y = this.position.y - (this.size / 2);
-
-    context.beginPath();
-    context.fillStyle = "rgba(255, 255, 255, .15)";
-    context.rect(x, y, this.size, this.size);
-    context.fill();
+    this.acceleration.x = 0;
+    this.acceleration.y = 0;
   }
 }
